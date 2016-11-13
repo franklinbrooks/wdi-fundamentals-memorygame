1ba7b43037f89d9dfbb22c2b591616d9b4b99379
Franklin Christopher Brooks <franklinchristopherbrooks@gmail.com>
2016-11-13 05:57:54 -0500

    Added js to create cards and moved script tag to bodyof html

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,27 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-
+/*
 if (cardTwo === cardFour) {
 	alert("You found a match!");
 }
 else {
 	alert("Sorry, try again.");
 }
+*/
+var gameBoard = document.getElementById('game-board');
+var createCards = function() {
+  
+  for (var i = 0; i < 4; i++) {
+    var cardElement = document.createElement('div'); // create a new div tag
+    cardElement.className = 'card'; // give the div a class name
+    gameBoard.appendChild(cardElement); // add the div to the game bord
+  }
+}
+
+createCards();
+
+
